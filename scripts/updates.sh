caca2cee1da8a115c2d4f97ccc5b8f708e022e26
huanillo <juanin92@gmail.com>
2025-06-01 19:57:55 +0200

    Changed battery icons, fixed bug with updates widget.

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -5,7 +5,7 @@ AUR_COUNT=$(yay -Qua 2>/dev/null | wc -l)
 TOTAL=$((PACMAN_COUNT + AUR_COUNT))
 
 if [[ $TOTAL -gt 0 ]]; then
-    echo "{\"text\": \"⬆ $TOTAL\", \"tooltip\": \"$PACMAN_COUNT del sistema y $AUR_COUNT de AUR\"}"
+    echo "{\"text\": \"⬆ $TOTAL\", \"tooltip\": \"$PACMAN_COUNT del sistema y $AUR_COUNT de AUR\", \"class\": \"updates-available\"}"
 else
     echo ""
 fi
